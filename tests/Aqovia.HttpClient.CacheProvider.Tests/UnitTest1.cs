18c562eb926f6e1fdf895b52052dc4e46279c756
husnus <husnus@interxion.com>
2021-09-28 18:15:45 +0100

    Updated yml file and unit test proj is added

---
+++ tests/Aqovia.HttpClient.CacheProvider.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Aqovia.HttpClient.CacheProvider.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
